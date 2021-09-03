053bc05acd243997633725a43b1dc32868a123c4
Dalilah Aguilar <dalilahaguilar9@gmail.com>
2021-09-03 13:23:59 -0500

    commit tree 5383b3ffe6997990ea4095455593d0ff00a7096f

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
+let num1 = 1;
+let num2 = 62;
+let multiply = num1 * num2
 
-// Write your code here
+function multiply(){
+return multiply;
+}
+
+
+let random = Math.floor(Math.random() * 10) 
+function random(){
+    return random;
+}
+
+let num3 = 22
+let num4 = 6
+let mod = num3 % num4
+function mod(){
+    return mod;
+}
+
+
+
+let max = Math.max(0,1,2,3,20)
+function max() {
+return max;
+}
+
